3dce81ef76e98d528dba382a9e62ca33b098064c
Anoyi <545544032@qq.com>
2021-12-18 15:01:54 +0800

    :white_check_mark: test cases

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -1,7 +1,8 @@
 from fastlab import logs
 
+# Python3 Logging
+# See more: https://docs.python.org/3/library/logging.html
 
-logs.debug('debug')
-logs.warn('warn')
-logs.info('info')
-logs.error('error')
+logs.warning('warn')    # 2021-12-18 14:23:31.000  WARNING 88493 --- [  MainThread] test_logs        : warn
+logs.info('info')       # 2021-12-18 14:23:31.000     INFO 88493 --- [  MainThread] test_logs        : info
+logs.error('error')     # 2021-12-18 14:23:31.000    ERROR 88493 --- [  MainThread] test_logs        : error
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,9 +2,9 @@ from fastlab.models import Response, PageData
 
 
 # Web base repsonse
-print(Response(data='hello'))                                   # >>> {"code": 0, "message": "", "data": "hello"}
-print(Response(code=10000, message='server error', data={}))    # >>> {"code": 10000, "message": "server error", "data": {}}
+assert Response(data='hello').dict() == {"code": 0, "message": "", "data": "hello"}
+assert Response(code=10000, message='server error', data={}) == {"code": 10000, "message": "server error", "data": {}}
 
 
-# Web pageable data: skip=0 limit=20 total=100 has_more=True data=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-print(PageData(skip=0, limit=20, total=100, has_more=True, data=list(range(0, 20))))    
+# Web pageable data
+assert PageData(skip=0, limit=10, total=100, has_more=True, data=list(range(0, 10))) == {"skip": 0 , "limit": 10, "total": 100, "has_more": True, "data": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}
