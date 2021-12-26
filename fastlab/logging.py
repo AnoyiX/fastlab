2cae005c9c3b81aceb2435548c9ba5087c01c4a8
Anoyi <545544032@qq.com>
2021-12-26 18:53:46 +0800

    :bug: fix milliseconds in logging time format

--- fastlab/logging.py
+++ fastlab/logging.py
@@ -4,7 +4,7 @@ logs = logging.getLogger('console')
 logs.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
-log_format = '%(asctime)s %(levelname)+8s %(process)d --- [%(threadName)+12s] %(module)-16s : %(message)s'
-formatter = logging.Formatter(log_format, datefmt='%Y-%m-%d %H:%M:%S.000')
+log_format = '%(asctime)s.%(msecs)03d %(levelname)+8s %(process)d --- [%(threadName)+12s] %(module)-16s : %(message)s'
+formatter = logging.Formatter(log_format, datefmt='%Y-%m-%d %H:%M:%S')
 handler.setFormatter(formatter)
 logs.addHandler(handler)
